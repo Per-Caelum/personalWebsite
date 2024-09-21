1de78d39b5acc8a8bdd693af37390206b5bb9f59
Per-Caelum <mzeraphina@gmail.com>
2024-09-21 06:49:23 -0500

    corrected the alignments of text for portfolio projects and about hobbies. Included the required changes for the contact page and javascript functionality for a submit success page.

--- javascript.js
+++ javascript.js
@@ -11,3 +11,13 @@ function doAlertThing() {
   alert(text);
   doConsoleThing();
 }
+
+/* Instruction-provided code */
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
